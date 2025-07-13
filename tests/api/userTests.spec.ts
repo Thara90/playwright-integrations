daceb3939a06348b15bb9b416cb9689b855117e8
Thara90 <tpshadinijk@gmail.com>
2025-07-13 19:35:32 +0530

    Add additional user login tests for invalid credentials

--- tests/api/userTests.spec.ts
+++ tests/api/userTests.spec.ts
@@ -17,3 +17,35 @@ test('Login with valid credenatials', async ({ request }) => {
   const response = await _response.json();
   console.log(response);
 });
+
+test('Login with invalid username and valid password', async ({ request }) => {
+  const userClient = new UserClient(request);
+
+  const credenatials = {
+    email: 'test@practicesoftwaretesting.com',
+    password: 'welcome01'
+  };
+
+  const _response = await userClient.createLogin(credenatials);
+  expect.soft(_response.status()).toBe(401);
+
+  const response = await _response.json();
+  expect(response.error).toBe("Unauthorized");
+  console.log(response);
+});
+
+test('Login with valid username and invalid password', async ({ request }) => {
+  const userClient = new UserClient(request);
+
+  const credenatials = {
+    email: 'test@practicesoftwaretesting.com',
+    password: 'test123'
+  };
+
+  const _response = await userClient.createLogin(credenatials);
+  expect.soft(_response.status()).toBe(401);
+
+  const response = await _response.json();
+  expect(response.error).toBe("Unauthorized");
+  console.log(response);
+});
