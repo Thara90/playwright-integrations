a2947e39606d6a8fe4437ae901bec3d4ff39ec11
Thara90 <tpshadinijk@gmail.com>
2025-07-16 17:02:44 +0530

    Enhance user registration tests: validate response fields

--- tests/api/user/user.post.spec.ts
+++ tests/api/user/user.post.spec.ts
@@ -10,10 +10,20 @@ test.describe('/users/register - POST endpoint validation', () => {
         const userData = await UserDataBuilder.validRequestBody();
         const requestData = fillRequestTemplate(registerUserTemplate, userData);
         const _response = await userClient.postRegister(requestData);
+
         expect.soft(_response.status()).toBe(201);
-        expect.soft(_response.ok()).toBeTruthy();
 
         const response = await _response.json();
+        expect.soft(response.first_name).toBe(userData.first_name);
+        expect.soft(response.last_name).toBe(userData.last_name);
+        expect.soft(response.phone).toBe(userData.phone);
+        expect.soft(response.dob).toBe(userData.dob);
+        expect.soft(response.email).toBe(userData.email);
+        expect.soft(response.address.street).toBe(userData.street);
+        expect.soft(response.address.city).toBe(userData.city);
+        expect.soft(response.address.state).toBe(userData.state);
+        expect.soft(response.address.country).toBe(userData.country);
+        expect.soft(response.address.postal_code).toBe(userData.postal_code);
         console.log(response);
     });
 
@@ -24,6 +34,10 @@ test.describe('/users/register - POST endpoint validation', () => {
         expect.soft(_response.status()).toBe(422);
 
         const response = await _response.json();
+        expect.soft(response.first_name[0]).toBe("The first name field is required.");
+        expect.soft(response.last_name[0]).toBe("The last name field is required.");
+        expect.soft(response.email[0]).toBe("The email field is required.");
+        expect.soft(response.password[0]).toBe("The password field is required.");
         console.log(response);
     });
 
@@ -34,6 +48,12 @@ test.describe('/users/register - POST endpoint validation', () => {
         expect.soft(_response.status()).toBe(422);
 
         const response = await _response.json();
+        expect.soft(response.password).toEqual([
+            'The password field must be at least 8 characters.',
+            'The password field must contain at least one uppercase and one lowercase letter.',
+            'The password field must contain at least one symbol.',
+            'The password field must contain at least one number.'
+        ]);
         console.log(response);
     });
 });
