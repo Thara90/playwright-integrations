f482071802c05d8faaceacd572c70be607ecd795
Thara90 <tpshadinijk@gmail.com>
2025-07-27 17:32:34 +0530

    client for brand endpoint

---
+++ resources/api/clients/brandClient.ts
@@ -0,0 +1,28 @@
+import { APIRequestContext, expect } from '@playwright/test';
+import { logRequest, logResponse } from '@utils/apiUtils';
+import { log } from 'console';
+
+export class ProductClient {
+  readonly request: APIRequestContext;
+  readonly baseUrl: string;
+
+  constructor(request: APIRequestContext) {
+    this.request = request;
+    this.baseUrl = process.env.API_URL!;
+  }
+
+  /* ----------- GET ENDPOINTS ----------- */
+
+  async getBrands() {
+    const url = `${this.baseUrl}/brands`;
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+    const response = await this.request.get(url, { headers });
+
+    logRequest('GET', url, headers);
+    await logResponse(response);
+    return response;
+  }
+
+}
