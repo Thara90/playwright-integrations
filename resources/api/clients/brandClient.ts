8aec05f993dc13d7ad158a5539f3696210700c3b
Thara90 <tpshadinijk@gmail.com>
2025-07-27 18:00:12 +0530

    getBrandbyId test casses

--- resources/api/clients/brandClient.ts
+++ resources/api/clients/brandClient.ts
@@ -25,4 +25,16 @@ export class BrandClient {
     return response;
   }
 
+  async getBrandById(brandId: string) {
+    const url = `${this.baseUrl}/brands/${brandId}`;
+    const headers = {
+      'Content-Type': 'application/json'
+    }
+    const response = await this.request.get(url, { headers });
+
+    logRequest('GET', url, headers);
+    await logResponse(response);
+    return response;
+  }
+
 }
--- tests/api/brand/brand.get.spec.ts
+++ tests/api/brand/brand.get.spec.ts
@@ -14,39 +14,23 @@ test.describe('/brands - Get endpoint validation', () => {
     });
 });
 
-// test.describe('/users/${userId} - Get endpoint validation', () => {
-//     let userId: string;
+test.describe('/brands/${brandId} - Get endpoint validation', () => {
 
-//     test('Retreive registered user', async ({ adminToken, userClient }) => {
-//         let userId: string;
+        test('Retreive brand by id', async ({ brandClient }) => {
+            const _response = await brandClient.getBrandById('01k15x3bvnncxhcy9ncpjsa3t4');
+            expect.soft(_response.status()).toBe(200);
+            expect.soft(_response.ok()).toBeTruthy();
+            const response = await _response.json();
+        });
 
-//         await test.step('Register an user', async () => {
-//             const { createdUser } = await createUser(userClient);
-//             userId = createdUser.id;
-//             console.log(`Registered user Id: ${userId}`);
-//         });
-//         await test.step('Retreive user', async () => {
-//             const _response = await userClient.getUserById(adminToken, userId);
-//             expect.soft(_response.status()).toBe(200);
-//             expect.soft(_response.ok()).toBeTruthy();
-//             const response = await _response.json();
-//             validateSchema(response, userResponseSchema);
-//         });
-//     });
+        test("Retreive user that doesn't exist", async ({ brandClient }) => {
+            const _response = await brandClient.getBrandById('nonExistentBrandId');
+            expect.soft(_response.status()).toBe(404);
+            const response = await _response.json();
+            expect.soft(response.message).toBe("Requested item not found");
+        });
 
-//     test("Retreive user that doesn't exist", async ({ adminToken, userClient }) => {
-//         const _response = await userClient.getUserById(adminToken, 'nonExistentUserId');
-//         expect.soft(_response.status()).toBe(404);
-//         const response = await _response.json();
-//     });
-
-//     test('Retreive user without authorizing', async ({ userClient }) => {
-//         const _response = await userClient.getUserById('invalidToken', '123');
-//         expect.soft(_response.status()).toBe(401);
-//         const response = await _response.json();
-//         expect.soft(response.message).toBe("Unauthorized");
-//     });
-// });
+    });
 
 // test.describe('/users/me - Get endpoint validation', () => {
 
